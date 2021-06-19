9f8c8aef6f9a600a40c526dd297c71847a90bfaa
longwatermelon <longwatermelon1@gmail.com>
2021-06-18 19:14:02 -0700

    speed up audio

--- nightcore.py
+++ nightcore.py
@@ -5,6 +5,8 @@ from xml.etree import ElementTree
 import random
 from bs4 import BeautifulSoup
 import shutil
+from pydub import AudioSegment
+
 
 def download_video(url):
     ydl_opts = {
@@ -34,6 +36,17 @@ def get_random_image():
     with open("image.png", "wb") as f:
         shutil.copyfileobj(image_data.raw, f) 
 
+def speedup_audio(path):
+    sound = AudioSegment.from_file(path)
+    new_sound = sound._spawn(sound.raw_data, overrides = {
+        "frame_rate": int(sound.frame_rate * 1.25)
+    })
+
+    return new_sound.set_frame_rate(sound.frame_rate)
 
 get_random_image()
 download_video("https://www.youtube.com/watch?v=9lNZ_Rnr7Jc")
+
+nightcore = speedup_audio("audio.mp3")
+nightcore.export("nightcore.mp3", format="mp3")
+
